d2f584e0f23025921cbaffe99d0fd23f8c963fa3
IKNCODE <66886403+IKNCODE@users.noreply.github.com>
2021-08-24 10:13:53 +0300

    Update display.py

--- display.py
+++ display.py
@@ -3,3 +3,15 @@ import sys
 import math 
 import time
 
+pygame.init()
+
+class display:
+    def __init__(self,width,height):
+        self.x == width
+        self.y == height
+    
+
+
+
+if __name__ == '__main__':
+    pass

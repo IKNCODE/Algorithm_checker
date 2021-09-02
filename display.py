e09a070ee92fe38582ea2b10fbca86abd60543ed
IKNCODE <66886403+IKNCODE@users.noreply.github.com>
2021-09-02 18:40:14 +0300

    Finishing classes Button and Display

--- display.py
+++ display.py
@@ -2,16 +2,47 @@ import pygame
 import sys
 import math 
 import time
+pygame.font.init()
+font = pygame.font.SysFont('arial', 20)
 
-pygame.init()
-
-class display:
+class Display:   
     def __init__(self,width,height):
-        self.x == width
-        self.y == height
-    
+        self.x = width
+        self.y = height
+        pygame.init()
+        global screen
+        screen = pygame.display.set_mode((self.x,self.y)) #Creating a pygame window with shown x and y
+
+
+class Button:
+    def __init__(self,text,pos,font,bg='white',feedback=''):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("arial", font)
+        if feedback == '':
+            self.feedback = 'text'
+        else:
+            self.feedback = feedback
+        self.change_text(text,bg)
+
+    def change_text(self,text,bg='green'):
+        self.text = self.font.render(text,1,pygame.Color('White'))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0,0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def show_btn(self):
+        screen.blit(self.surface, (self.x, self.y))
+        pygame.display.update()
 
 
 
 if __name__ == '__main__':
-    pass
+    d = Display(500, 600)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        btn1 = Button("Hello world", (100,100),font=30,bg='white')
+        btn1.show_btn()
